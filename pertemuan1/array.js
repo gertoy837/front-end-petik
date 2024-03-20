ee62a1724374e27347f0d187f8264b86f9e6379e
gertoy837 <dikamahardika837@gmail.com>
2024-03-20 11:14:32 +0700

    Praktikum pertemuan 1 - Javascript Dasar

---
+++ pertemuan1/array.js
@@ -0,0 +1,13 @@
+// Membuat array: [] & array()
+
+const fruits = ["apel", "Anggur", "Jeruk"];
+
+
+// Mengakses nilai
+console.log(fruits[0]);
+
+// Looping
+for (let i = 0; i < fruits.length; i++) console.log(fruits[i]);
+
+// Looping for-of
+for (const fruit of fruits) console.log(fruit);
---
+++ pertemuan1/data-types.js
@@ -0,0 +1,9 @@
+const name = "Ucup";
+const age = 28;
+// setiap awal type boolean maka di awala dengan kata (is)
+const isMarried = false;
+let tglLahir = '2004-02-10';
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isMarried);
+console.log(typeof tglLahir);
